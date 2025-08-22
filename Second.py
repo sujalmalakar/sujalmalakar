eded8db00d546f0b637cf20ca47fa3fb77830135
sujalmalakar <manandharsujal75@gmail.com>
2025-08-22 15:38:11 +0300

    Add files via upload

---
+++ Second.py
@@ -0,0 +1,9 @@
+marks1 = int(input("enter your marks in pshyics"))
+marks2 = int(input("enter your marks in maths"))
+marks3 = int(input("enter your marks in programming"))
+total = ((marks1 + marks2 + marks3)/225)*100
+print(total)
+int_total = int(total)
+print(int_total)
+
+
